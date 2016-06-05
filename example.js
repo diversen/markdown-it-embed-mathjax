d900bb20531393699f7e1e5cfe3eb47230a25adc
dennis iversen <dennis.iversen@gmail.com>
2016-06-05 17:00:58 +0200

    remove mathjax

---
+++ example.js
@@ -0,0 +1,5 @@
+md = require('./index');
+md.options.html = false;
+
+var str = "<p>Esacpe html in this example: A embeded video. ![test](./test/test.mp4) Some mathjax $1 *2* 3$";
+console.log(md.render(str));
--- test.js
+++
@@ -1,5 +0,0 @@
-md = require('./index');
-md.options.html = true;
-
-var str = "<p>test</p># her er en test ![test](./test/test.mp4) $1 *2* 3$";
-console.log(md.render(str));
